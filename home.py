7dca9ad440cc34cd4a12ef65beae2387229e95a0
鈴木亜樹 <suzukiaki@AkMac.local>
2020-03-24 18:31:32 +0900

    Add Flask-WTF Form

--- home.py
+++ home.py
@@ -4,9 +4,13 @@ Top Page
 """
 
 #=====import
-from flask import Flask ,render_template
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from form import RegistrationForm, LoginForm
 #=====import EOF
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '311a2873aeb05edd55fd2ff4b0b605d1'
 #=====html template
 # home.htmlに渡すデータ
 posts = [
@@ -33,6 +37,33 @@ def home():
 @app.route('/about')
 def about():
   return render_template('about.html',title='About')
+
+
+# ----- 追加 -----
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # POSTリクエスト(登録時)
+        flash('Account created for %s!' % form.username.data, 'success')
+        return redirect(url_for('home'))
+        # 初期表示時
+    return render_template('register.html', title='Register', form=form)
+
+
+# ----- 追加 -----
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # POSTリクエスト(ログイン時)
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+            # 初期表示時
+    return render_template('login.html', title='Login', form=form)
 
 #pythonファイルを直に指定してWebサーバを起動させるための宣言
 if __name__ == '__main__':
